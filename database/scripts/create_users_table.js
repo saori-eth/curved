d2f32cdf8cac8d274e1dacb82c1553fe567d82a3
philbert burrow <75766943+philburrrt@users.noreply.github.com>
2023-09-17 03:47:33 -0500

    ⭐ create users table & script

--- database/scripts/create_users_table.js
+++ database/scripts/create_users_table.js
@@ -5,19 +5,21 @@ const db = new DB();
   await db.createTable(
     "users",
     `
-      id INTEGER PRIMARY KEY,
-      username TEXT NOT NULL UNIQUE,
-      address TEXT NOT NULL UNIQUE,
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      address TEXT NOT NULL,
       createdAt TEXT NOT NULL,
-      updatedAt TEXT NOT NULL
+      updatedAt TEXT NOT NULL,
+      avatar TEXT NOT NULL
     `
   );
 
   await db.insert("users", {
-    username: "user1",
-    address: "0x123",
+    username: "admin",
+    address: "0x00000",
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
+    avatar: "https://i.imgur.com/3GvwN8w.png",
   });
 
   await db.fetchAll("users").then((rows) => console.log(rows));
