86fd58eefe2f45de4878159427eb67750ee72550
philburrrt <philbertburrow@protonmail.com>
2023-09-18 15:30:32 -0500

    ⭐ init dropzone

--- app/components/Body.jsx
+++ app/components/Body.jsx
@@ -1,7 +1,18 @@
+"use client";
+import { Dropzone } from "./Dropzone";
+
 export const Body = ({ children }) => {
+  const handleDrop = (file) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      console.log("blob: ", reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-4 px-4">
-      {children}
+      <Dropzone onDrop={handleDrop}>{children}</Dropzone>
     </div>
   );
 };
---
+++ app/components/Dropzone.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export const Dropzone = ({ onDrop, children }) => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+
+    if (e.dataTransfer.items) {
+      for (let i = 0; i < e.dataTransfer.items.length; i++) {
+        if (e.dataTransfer.items[i].kind === "file") {
+          const file = e.dataTransfer.items[i].getAsFile();
+          onDrop(file);
+        }
+      }
+    }
+  };
+
+  return (
+    <div
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+      style={{
+        height: "100vh",
+        width: "100vw",
+        border: isDragging ? "2px dashed red" : "2px dashed gray",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Dropzone;
