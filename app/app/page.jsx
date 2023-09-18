868822eaa13178f0dfc75a2ae3d2c89b82040dc8
philbert burrow <75766943+philburrrt@users.noreply.github.com>
2023-09-18 02:14:01 -0500

    🔨 reset index page

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,27 +1,7 @@
 "use client";
-import { useAccount, useSignMessage } from "wagmi";
-import { useEffect } from "react";
 
 const Page = () => {
-  const account = useAccount();
-  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
-    message: `
-          Sign this message to login to Curved Social.
-          Your address is ${account.address}
-        `,
-  });
-
-  useEffect(() => {}, [content]);
-
-  return (
-    <div>
-      <button disabled={isLoading} onClick={() => signMessage()}>
-        Sign message
-      </button>
-      {isSuccess && <div className="break-all">Signature: {data}</div>}
-      {isError && <div>Error signing message</div>}
-    </div>
-  );
+  return <></>;
 };
 
 export default Page;
