7985f27f7ce3de56c5c6fe9fc7ba77f00479ada9
philbert burrow <75766943+philburrrt@users.noreply.github.com>
2023-09-18 04:49:40 -0500

    ⭐ new logo

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,13 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
 
 export const Header = () => {
   return (
-    <div className="flex justify-end p-3">
+    <div className="flex justify-between p-3">
+      <div className="flex items-center">
+        <Image src="/logo.svg" alt="Curved Logo" width={30} height={30} />
+        <h1 className="text-2xl font-bold">Curved</h1>
+      </div>
       <ConnectButton accountStatus="address" showBalance={false} />
     </div>
   );
