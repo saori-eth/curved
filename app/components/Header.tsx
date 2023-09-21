de4269d1be98f163acfd67d1e3b6d40dcf7275d6
Kayh <basedlainpilled@gmail.com>
2023-09-21 12:01:59 -0400

    set fixed header height

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <nav className="fixed inset-x-0 top-0 z-10 flex select-none justify-center bg-neutral-700/90 p-3 backdrop-blur-sm">
-      <div className="flex w-full max-w-6xl justify-between">
+    <nav className="fixed inset-x-0 top-0 z-10 flex h-16 select-none items-center justify-center bg-neutral-700/90 backdrop-blur-sm">
+      <div className="flex h-min w-full max-w-6xl justify-between">
         <Link href="/" className="flex cursor-pointer items-center space-x-1">
           <Image
             src="/logo.svg"
@@ -13,6 +13,7 @@ export function Header() {
             width={30}
             height={30}
             draggable={false}
+            priority
           />
           <h1 className="text-2xl font-bold">Curved</h1>
         </Link>
