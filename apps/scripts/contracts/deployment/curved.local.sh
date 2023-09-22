515d4ddaf97f0aaae9a946e00331ebac1bda2a48
philburrrt <philbertburrow@protonmail.com>
2023-09-22 06:55:27 -0500

    🧹 refactor deployment script for monorepo

--- apps/scripts/contracts/deployment/curved.local.sh
+++ apps/scripts/contracts/deployment/curved.local.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source .env &&
-cd contracts &&
 forge create --rpc-url "http://localhost:8545" \
     --constructor-args $LOCAL_DEPLOYER_ADDRESS "50000000000000000" \
     --private-key $LOCAL_PRIVATE_KEY \
