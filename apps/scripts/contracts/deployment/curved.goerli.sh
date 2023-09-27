3658ff10f7198da971788b2dc6c5e86feebdb84b
Kayh <basedlainpilled@gmail.com>
2023-09-27 07:10:20 -0400

    update deploy script to use new env

--- apps/scripts/contracts/deployment/curved.goerli.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-forge create --rpc-url $GOERLI_HTTP_URL \
-    --constructor-args $GOERLI_DEPLOYER_ADDRESS "25000000000000000" \
-    --private-key $GOERLI_PRIVATE_KEY \
-    src/Curved.sol:Curved
--- apps/scripts/contracts/deployment/curved.local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-forge create --rpc-url "http://localhost:8545" \
-    --constructor-args $LOCAL_DEPLOYER_ADDRESS "50000000000000000" \
-    --private-key $LOCAL_PRIVATE_KEY \
-    src/Curved.sol:Curved
