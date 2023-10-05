6710f3d95b4fc8c074084b2403002e1a63fbb3f0
philbert burrow <75766943+philburrrt@users.noreply.github.com>
2023-10-05 13:32:05 -0700

    only ask for noti if signed in

--- apps/client/app/ClientWrapper.tsx
+++ apps/client/app/ClientWrapper.tsx
@@ -6,11 +6,12 @@ import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { useEffect } from "react";
 import { WagmiConfig } from "wagmi";
 
-import { listenToChanges,sub } from "@/lib/push";
+import { listenToChanges, sub } from "@/lib/push";
 
 import AuthProvider from "./AuthProvider";
 import { RainbowKitAuthProvider } from "./RainbowkitAuthProvider";
 import { chains, config } from "./wagmi";
+import { useAccount } from "wagmi";
 
 const theme = darkTheme({
   accentColor: "#7dd3fc",
@@ -25,20 +26,25 @@ interface Props {
 }
 
 export default function ClientWrapper({ children }: Props) {
-  useEffect(() => {
-    sub(); // try to subscribe to push notifications
-    listenToChanges(); // listen to changes in the push notifications
-  }, []);
-
   return (
     <WagmiConfig config={config}>
       <AuthProvider>
         <RainbowKitAuthProvider>
           <RainbowKitProvider theme={theme} chains={chains}>
-            {children}
+            <NotificationWrapper>{children}</NotificationWrapper>
           </RainbowKitProvider>
         </RainbowKitAuthProvider>
       </AuthProvider>
     </WagmiConfig>
   );
 }
+
+const NotificationWrapper = ({ children }: Props) => {
+  const { address } = useAccount();
+  useEffect(() => {
+    if (!address) return;
+    sub();
+    listenToChanges();
+  }, []);
+  return <>{children}</>;
+};
