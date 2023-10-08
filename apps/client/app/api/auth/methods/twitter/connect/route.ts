6ea6a2fff19b6bcc53a21338833052327bcbaabf
Kayh <basedlainpilled@gmail.com>
2023-10-08 17:30:39 -0400

    lint

--- apps/client/app/api/auth/methods/twitter/connect/route.ts
+++ apps/client/app/api/auth/methods/twitter/connect/route.ts
@@ -18,6 +18,7 @@ export async function GET() {
     httpOnly: true,
     maxAge: 60 * 60,
     name: "twitter-code-verifier",
+    path: "/",
     value: codeVerifier,
   });
 
@@ -25,6 +26,7 @@ export async function GET() {
     httpOnly: true,
     maxAge: 60 * 60,
     name: "twitter-state",
+    path: "/",
     value: state,
   });
 
