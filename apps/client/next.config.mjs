a884596c050959ad5d2c7da1abaf8ec1d9964bc5
philburrrt <philbertburrow@protonmail.com>
2023-10-08 15:28:21 -0500

    re-enable pwa

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+const withPWA = withPWAInit({
+  dest: "public",
+});
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -11,4 +15,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
