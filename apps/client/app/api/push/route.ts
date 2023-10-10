5da812d6398cd9e60994ffdc0340f024fbf8521e
philburrrt <philbertburrow@protonmail.com>
2023-10-10 14:58:39 -0500

    tolowercase :(

--- apps/client/app/api/push/route.ts
+++ apps/client/app/api/push/route.ts
@@ -1,11 +1,14 @@
 import { sendNotification } from "@/lib/push/webpush";
 export function GET() {
-  sendNotification("0x659b8aC78bf3428077529ef9E1cCa22B1d711FFD", {
-    body: "Hello, world!",
-    icon: "/images/android-chrome-192x192.png",
-    title: "Hello",
-    url: "/",
-  });
+  sendNotification(
+    "0x659b8aC78bf3428077529ef9E1cCa22B1d711FFD".toLocaleLowerCase(),
+    {
+      body: "Hello, world!",
+      icon: "/images/android-chrome-192x192.png",
+      title: "Hello",
+      url: "/",
+    },
+  );
 
   return new Response("OK");
 }
