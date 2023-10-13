d184ffe370f3879d68a894ed1856395ef3de30a6
Kayh <basedlainpilled@gmail.com>
2023-10-13 08:06:26 -0400

    remove scroll to top

--- apps/client/app/(feed)/PostFeed.tsx
+++ apps/client/app/(feed)/PostFeed.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
-
 import { PostCard } from "@/components/PostCard";
 
 import { useFeed } from "./FeedContext";
 
 export function PostFeed() {
   const { posts } = useFeed();
-
-  useEffect(() => {
-    // On refresh, scroll to the top of the page
-    window.scrollTo(0, 0);
-  }, []);
 
   return (
     <>
