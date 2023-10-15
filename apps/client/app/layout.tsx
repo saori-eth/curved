d277ad7e7b3be28c7b24b9a6f23cd458c4357594
philburrrt <philbertburrow@protonmail.com>
2023-10-14 19:11:17 -0500

    block niggas

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -7,6 +7,10 @@ import { Gemunu_Libre } from "next/font/google";
 import { baseMetadata } from "./baseMetadata";
 import { Sidebar } from "./Sidebar";
 
+import { env } from "@/lib/env.mjs";
+
+const mode = env.NEXT_PUBLIC_NODE_ENV;
+
 const ClientWrapper = dynamic(() => import("./ClientWrapper"));
 
 const font = Gemunu_Libre({
@@ -27,6 +31,26 @@ export default function RootLayout({ children }: Props) {
     <html lang="en" className={`overflow-y-scroll ${font.className}`}>
       <body className="bg-slate-800 text-white">
         <ClientWrapper>
+          {mode !== "development" && (
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "rgba(0,0,0,0.8)",
+                zIndex: 9999,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "#fff",
+                fontSize: "32px",
+              }}
+            >
+              Coming Soon
+            </div>
+          )}
           <div className="max-w-content relative mx-auto pb-16 md:grid md:grid-cols-7 md:gap-4 md:pb-0 md:pt-14">
             <div className="col-span-2">
               <Sidebar />
